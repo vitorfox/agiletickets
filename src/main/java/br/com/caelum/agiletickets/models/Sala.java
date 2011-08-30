efad0f701bd05254213bda4b20ba8a0aa79e3621
Vitor Fox <vitorfox@gmail.com>
2011-08-30 19:00:41 -0300

    Add Sala

--- src/main/java/br/com/caelum/agiletickets/models/Sala.java
+++ src/main/java/br/com/caelum/agiletickets/models/Sala.java
@@ -1,8 +1,6 @@
 package br.com.caelum.agiletickets.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 @Entity
 public class Sala {
